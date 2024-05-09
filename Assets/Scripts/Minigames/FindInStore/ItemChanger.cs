c3ac137bf0ff7d88413a2a60865b3e8a11a422e6
Sander "Sandal" Bex <sanderbex12@gmail.com>
2024-05-10 01:58:36 +0200

    fixed my little mistake
    there are no longer build errors

--- Assets/Scripts/Minigames/FindInStore/ItemChanger.cs
+++ Assets/Scripts/Minigames/FindInStore/ItemChanger.cs
@@ -102,7 +102,7 @@ public class ItemChanger : MonoBehaviour
         //gets winning players
         Player[] players = (FindObjectsOfType<Player>()).OrderBy(i => i._score).ToArray();
 
-        writer.Write("234:" + players[0]._playerID + players[1]._playerID + players[2]._playerID + players[3]._playerID);
+        writer.Write("234:" + players[0]._playerID + "," + players[1]._playerID + "," + players[2]._playerID + "," + players[3]._playerID);
         Debug.Log(writer.ToString());
         writer.Close();
 
