efe91e202e8373d0e70557cdf90baa764cde6580
Evy <os.zadroga@hotmail.com>
2024-05-13 11:53:30 +0200

    Race code array use and clean up.

--- Assets/Scripts/Minigames/MashRace/MashRace.cs
+++ Assets/Scripts/Minigames/MashRace/MashRace.cs
@@ -6,18 +6,12 @@ public class MashRace : MonoBehaviour
 {
     private float speed = 10f;
 
-    private bool moveIsActive = false;
-    private bool moveIsActive1 = false;
-    private bool moveIsActive2 = false;
-    private bool moveIsActive3 = false;
+    private float distanceToMove = 7.5f;
+    private float timerMove = 0.2f;
 
-    private float distanceToMove = 5f;
-    private float timerMove = 0.5f;
-
-    private Vector3 targetPosition;
-    private Vector3 targetPosition1;
-    private Vector3 targetPosition2;
-    private Vector3 targetPosition3;
+    private Vector3[] targetPositions = new Vector3[4];
+    private bool[] playersFinished = new bool[4];
+    private bool[] moveIsActive = new bool[4];
     private bool _winIsActive = false;
 
     public bool startDelayBeforeMainBoard = false;
@@ -50,142 +44,93 @@ public class MashRace : MonoBehaviour
     }
     private void Movement()
     {
-        if (!moveIsActive && Input.GetButtonDown("AButton1"))
+        for (int i = 0; i < 4; i++)
         {
-            GameObject player1 = GameObject.FindGameObjectWithTag("Player/1");
-            targetPosition = player1.transform.position + Vector3.up * distanceToMove;
-            moveIsActive = true;
-            Invoke("CanMove1", timerMove);
-        }
-        else if (moveIsActive)
-        {
-            GameObject player1 = GameObject.FindGameObjectWithTag("Player/1");
-            player1.transform.position = Vector3.MoveTowards(player1.transform.position, targetPosition, speed * Time.deltaTime);
-            if (player1.transform.position == targetPosition)
+            if (!moveIsActive[i] && Input.GetButtonDown($"AButton{i + 1}"))
             {
-                moveIsActive = false;
+                GameObject player = GameObject.FindGameObjectWithTag($"Player/{i + 1}");
+                targetPositions[i] = player.transform.position + Vector3.up * distanceToMove;
+                moveIsActive[i] = true;
+                Invoke($"CanMove{i}", timerMove);
+            }
+            else if (moveIsActive[i])
+            {
+                GameObject player = GameObject.FindGameObjectWithTag($"Player/{i + 1}");
+                player.transform.position = Vector3.MoveTowards(player.transform.position, targetPositions[i], speed * Time.deltaTime);
+                if (player.transform.position == targetPositions[i])
+                {
+                    moveIsActive[i] = false;
+                    CanMove0();
+                    CanMove1();
+                    if (!playersFinished[i])
+                    {
+                        playersFinished[i] = true;
+                    }
+                }
             }
         }
-        if (!moveIsActive1 && Input.GetButtonDown("AButton2"))
-        {
-            GameObject player2 = GameObject.FindGameObjectWithTag("Player/2");
-            targetPosition1 = player2.transform.position + Vector3.up * distanceToMove;
-            moveIsActive1 = true;
-            Invoke("CanMove2", timerMove);
-        }
-        else if (moveIsActive1)
-        {
-            GameObject player2 = GameObject.FindGameObjectWithTag("Player/2");
-            player2.transform.position = Vector3.MoveTowards(player2.transform.position, targetPosition1, speed * Time.deltaTime);
-            if (player2.transform.position == targetPosition1)
-            {
-                moveIsActive1 = false;
-            }
-        }
-        if (!moveIsActive2 && Input.GetButtonDown("AButton3"))
-        {
-            GameObject player3 = GameObject.FindGameObjectWithTag("Player/3");
-            targetPosition2 = player3.transform.position + Vector3.up * distanceToMove;
-            moveIsActive2 = true;
-            Invoke("CanMove3", timerMove);
-        }
-        else if (moveIsActive2)
-        {
-            GameObject player3 = GameObject.FindGameObjectWithTag("Player/3");
-            player3.transform.position = Vector3.MoveTowards(player3.transform.position, targetPosition2, speed * Time.deltaTime);
-            if (player3.transform.position == targetPosition2)
-            {
-                moveIsActive2 = false;
-            }
-        }
-        if (!moveIsActive3 && Input.GetButtonDown("AButton4"))
-        {
-            GameObject player4 = GameObject.FindGameObjectWithTag("Player/4");
-            targetPosition3 = player4.transform.position + Vector3.up * distanceToMove;
-            moveIsActive3 = true;
-            Invoke("CanMove4", timerMove);
-        }
-        else if (moveIsActive3)
-        {
-            GameObject player4 = GameObject.FindGameObjectWithTag("Player/4");
-            player4.transform.position = Vector3.MoveTowards(player4.transform.position, targetPosition3, speed * Time.deltaTime);
-            if (player4.transform.position == targetPosition3)
-            {
-                moveIsActive3 = false;
-            }
-        }
+    }
+    private void CanMove0()
+    {
+        moveIsActive[0] = false;
     }
     private void CanMove1()
     {
-        moveIsActive = false;
-    }
-    private void CanMove2()
-    {
-        moveIsActive1 = false;
-    }
-    private void CanMove3()
-    {
-        moveIsActive2 = false;
-    }
-    private void CanMove4()
-    {
-        moveIsActive3 = false;
+        moveIsActive[1] = false;
     }
     private void OnTriggerEnter(Collider hit)
     {
-        if (hit.CompareTag("FinishLine/1"))
-        {
-            hudRaceScript.win1.text = "Player 1 Wins!";
-            hudRaceScript.win1.enabled = true;
-            _winIsActive = true;
+        if (_winIsActive) return;
 
-            Invoke("GameOver", 4f);
-        }
-        else if (hit.CompareTag("FinishLine/2"))
+        for (int i = 0; i < 4; i++)
         {
-            hudRaceScript.win2.text = "Player 2 Wins!";
-            hudRaceScript.win2.enabled = true;
-            _winIsActive = true;
-
-            Invoke("GameOver", 4f);
-        }
-        else if (hit.CompareTag("FinishLine/3"))
-        {
-            hudRaceScript.win3.text = "Player 3 Wins!";
-            hudRaceScript.win3.enabled = true;
-            _winIsActive = true;
-
-            Invoke("GameOver", 4f);
-        }
-        else if (hit.CompareTag("FinishLine/4"))
-        {
-            hudRaceScript.win4.text = "Player 4 Wins!";
-            hudRaceScript.win4.enabled = true;
-            _winIsActive = true;
-
-            Invoke("GameOver", 4f);
-        }
-        if (_winIsActive)
-        {
-            if (hudRaceScript.win2.enabled == true || hudRaceScript.win3.enabled == true || hudRaceScript.win4.enabled == true)
+            if (hit.CompareTag($"FinishLine/{i + 1}"))
             {
-                hudRaceScript.loose1.text = "Player 1 Looses";
-                hudRaceScript.loose1.enabled = true;
+                string winText = $"Player {i + 1} Wins!";
+                switch (i)
+                {
+                    case 0:
+                        hudRaceScript.win1.text = winText;
+                        hudRaceScript.win1.enabled = true;
+                        break;
+                    case 1:
+                        hudRaceScript.win2.text = winText;
+                        hudRaceScript.win2.enabled = true;
+                        break;
+                    case 2:
+                        hudRaceScript.win3.text = winText;
+                        hudRaceScript.win3.enabled = true;
+                        break;
+                    case 3:
+                        hudRaceScript.win4.text = winText;
+                        hudRaceScript.win4.enabled = true;
+                        break;
+                }
+                _winIsActive = true;
+                Invoke("GameOver", 3f);
             }
-            if (hudRaceScript.win1.enabled == true || hudRaceScript.win3.enabled == true || hudRaceScript.win4.enabled == true)
+            else
             {
-                hudRaceScript.loose2.text = "Player 2 Looses";
-                hudRaceScript.loose2.enabled = true;
-            }
-            if (hudRaceScript.win2.enabled == true || hudRaceScript.win1.enabled == true || hudRaceScript.win4.enabled == true)
-            {
-                hudRaceScript.loose3.text = "Player 3 Looses";
-                hudRaceScript.loose3.enabled = true;
-            }
-            if (hudRaceScript.win2.enabled == true || hudRaceScript.win3.enabled == true || hudRaceScript.win1.enabled == true)
-            {
-                hudRaceScript.loose4.text = "Player 4 Looses";
-                hudRaceScript.loose4.enabled = true;
+                string loseText = $"Player {i + 1} Looses!";
+                switch (i)
+                {
+                    case 0:
+                        hudRaceScript.loose1.text = loseText;
+                        hudRaceScript.loose1.enabled = true;
+                        break;
+                    case 1:
+                        hudRaceScript.loose2.text = loseText;
+                        hudRaceScript.loose2.enabled = true;
+                        break;
+                    case 2:
+                        hudRaceScript.loose3.text = loseText;
+                        hudRaceScript.loose3.enabled = true;
+                        break;
+                    case 3:
+                        hudRaceScript.loose4.text = loseText;
+                        hudRaceScript.loose4.enabled = true;
+                        break;
+                }
             }
         }
     }
