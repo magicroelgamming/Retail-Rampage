d78d3af214a20bbf4d83db72a78a00f5bfb1c540
Sander "Sandal" Bex <sanderbex12@gmail.com>
2024-05-29 20:48:14 +0200

    Made the ProductFall values a little slower
    face it the bombs were a little TOO fast...

--- Assets/Scripts/Minigames/ProductFall/BombScript.cs
+++ Assets/Scripts/Minigames/ProductFall/BombScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BombScript : MonoBehaviour
 {
-    private float fallSpeed = 20f;
+    private float fallSpeed = 12f;
     void Update()
     {
         transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
